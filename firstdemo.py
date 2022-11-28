369ab70e87be4f2b7e1e88ad14267d9315cb4e1d
dheemahee shah <dheeshah99@gmail.com>
2022-11-28 10:57:04 -0600

    first demo

---
+++ firstdemo.py
@@ -0,0 +1,72 @@
+# my first program
+# print("I love python!")
+"""first_name="Dheemahee"
+
+ctrl+shift+/ for multiple lines command
+
+
+last_name="Shah"
+print(first_name + " " + last_name)
+print(first_name)
+print(last_name)
+print(last_name + " " + first_name)
+f_name=input("enter you first name!")
+s_name=input("enter your last name!")
+print(f_name + " " + s_name + " " + "good morning!")
+
+sec= int(input("enter the number of sec you want"))
+days=int(input("enter the number of days you want"))
+hour=int(input("enter the number of hours you want"))
+min=int(input("enter the number of minutes you want"))
+print(sec*days*hour*min)
+
+i=10
+d=3.14141
+s="i am a string"
+print("%d\t%f\t%s"%(i,d,s))
+print("\nnewline\n")
+print("no newline")
+a=10
+b=20
+a += 1
+b -= 3
+print(b)
+print(a)
+
+#lets start the list progeam
+
+bicycle=['trek','cannondale','redline','specilized']
+print(bicycle[0])
+print(bicycle[1].title())
+print(bicycle[-1])
+#lets print the message for the user.....
+#here the f is used as string snd the message is taken as the variable
+message=f"my first bicycle was a {bicycle[0].title()}"
+print(message)
+
+# 3.1 programe
+
+names=['dhee','mahi','shah']
+
+print('\n'.join(names))
+#3.2 progrme
+message=f"welcome to my party {names[0].title()}"
+message1=f"welcome to my party {names[1].title()}"
+message2=f"welcome to my party {names[2].title()}"
+print("\n")
+print(message)
+print(message1)
+print(message2)
+#3.3
+cars=['honda','swift','audi','maruti']
+print(cars)
+print('\n'.join(cars))"""
+
+m=888
+h=60
+print(m//h)
+print(m%h)
+
+distance=7425
+aveg=550
+print(float(distance/aveg))
